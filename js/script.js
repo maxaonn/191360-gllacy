dfc73e4d12f7626b8f5da30526e45f489cf16812
maxaonn <annaseb@ya.ru>
2017-07-06 18:35:46 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 var link = document.querySelector(".js-modal-window-opener");
 var popup = document.querySelector(".modal-window");
+var popupToShake = document.querySelector(".modal-content")
 var close = document.querySelector(".modal-content-close");
 var form = popup.querySelector("form");
 var firstLine = popup.querySelector("[name=name]");
@@ -15,16 +16,16 @@ link.addEventListener("click", function(evt) {
 close.addEventListener("click", function(evt) {
 		evt.preventDefault();
 		popup.classList.remove("modal-window-open");
-    popup.classList.remove("modal-window-shaker");
+    popupToShake.classList.remove("modal-window-shaker");
 		}
 );
 
 form.addEventListener("submit", function(evt) {
 		if ( !firstLine.value || !secondLine.value) {
 				evt.preventDefault();
-				popup.classList.remove("modal-window-shaker");
-        popup.offsetWidth = popup.offsetWidth;
-        popup.classList.add("modal-window-shaker");
+				popupToShake.classList.remove("modal-window-shaker");
+        popupToShake.offsetWidth = popupToShake.offsetWidth;
+        popupToShake.classList.add("modal-window-shaker");
 		};
 });
 
@@ -32,7 +33,7 @@ window.addEventListener("keydown", function(evt) {
   if (event.keyCode === 27) {
     if (popup.classList.contains("modal-window-open")) {
           popup.classList.remove("modal-window-open");
-          popup.classList.remove("modal-window-shaker");
+          popupToShake.classList.remove("modal-window-shaker");
           };
       };
     });
