45fbe6e4ff7b5686beea460bf730a6e1bdb585fc
maxaonn <annaseb@ya.ru>
2017-07-06 17:38:03 +0300

    Create script.js

---
+++ js/script.js
@@ -0,0 +1,26 @@
+var link = document.querySelector(".location-details a");
+var popup = document.querySelector(".modal-window");
+var close = document.querySelector(".modal-content-close");
+var form = popup.querySelector("form");
+var firstLine = popup.querySelector("[name=name]");
+var secondLine = popup.querySelector("[name=email]");
+
+link.addEventListener("click", function(evt) {
+		evt.preventDefault();
+		popup.classList.add("modal-window-open");
+		firstLine.focus();
+		}
+);
+
+close.addEventListener("click", function(evt) {
+		evt.preventDefault();
+		popup.classList.remove("modal-window-open");
+		}
+);
+
+form.addEventListener("submit", function(evt) {
+		if ( !firstLine.value || !secondLine.value) {
+				evt.preventDefault();
+				popup.classList.add("modal-window-shaker");
+		};
+};
